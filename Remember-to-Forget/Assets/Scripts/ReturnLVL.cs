5784619edcc796b9ef052254d2d0fd89b8790003
Zares19 <garrett1shooter@gmail.com>
2023-05-03 23:17:30 -0500

    S

--- Remember-to-Forget/Assets/Scripts/ReturnLVL.cs
+++ Remember-to-Forget/Assets/Scripts/ReturnLVL.cs
@@ -7,6 +7,6 @@ public class ReturnLVL : MonoBehaviour
 {
     void OnTriggerEnter(Collider ChangeScene)
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Win");
     }
 }
